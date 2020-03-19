1ffdbb6399275c4b4b2966f8db812c4a71943c32
Pedromenegussi <pedrovipmenegussi@gmail.com>
2020-03-18 23:44:42 -0300

    upei o arquivo melhorado

--- acesso.py
+++ acesso.py
@@ -1,55 +1,60 @@
-import re
-try:
-    REGRESSIVOS = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+import re as regular_expression
 
-    def apenas_numeros(cnpj):
-        return re.sub(r'[^0-9]', '', cnpj)
+REGRESSIVOS = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 
-    def valida(cnpj):
-        cnpj = apenas_numeros(cnpj)
+def apenas_numeros(cnpj):
+    return regular_expression.sub(r'[^0-9]', '', cnpj)
 
+def valida(cnpj):
+    cnpj = apenas_numeros(cnpj)
+    try:
         if eh_sequencia(cnpj):
-            print(f'{cnpj}: É uma sequência!')
             return False
+    except:
+        return False
 
+    try:
         novo_cnpj = calcula_digito(cnpj, digito=1)
         novo_cnpj = calcula_digito(novo_cnpj, digito=2)
+    except Exception as e:
+        return False
 
-        if novo_cnpj == cnpj:
-            print(f'{cnpj} : Válido')
-        else:
-            print(f'{cnpj}: CNPJ inválido')
-    def calcula_digito(cnpj, digito):
-        if digito == 1:
-            regressivos = REGRESSIVOS[1:]
-            novo_cnpj = cnpj[:-2]
-        elif digito == 2:
-            regressivos = REGRESSIVOS
-            novo_cnpj = cnpj
-        else:
-            return None
+    if novo_cnpj == cnpj:
+        return True
+    else:
+        return False
 
-        total = 0
-        for indice, regressivo in enumerate(regressivos):
-            total += int(cnpj[indice]) * regressivo
+def calcula_digito(cnpj, digito):
+    if digito == 1:
+        regressivos = REGRESSIVOS[1:]
+        novo_cnpj = cnpj[:-2]
+    elif digito == 2:
+        regressivos = REGRESSIVOS
+        novo_cnpj = cnpj
+    else:
+        return None
 
-        digito = 11 -(total%11)
-        digito = digito if digito <= 9 else 0
+    total = 0
+    for indice, regressivo in enumerate(regressivos):
+        total += int(cnpj[indice]) * regressivo
 
-        return f'{novo_cnpj}{digito}'
+    digito = 11 -(total%11)
+    digito = digito if digito <= 9 else 0
 
-    def eh_sequencia(cnpj):
-        sequencia = cnpj[0] * len(cnpj)
+    return f'{novo_cnpj}{digito}'
 
-        if sequencia == cnpj:
-            return True
-        else:
-            return False
+def eh_sequencia(cnpj):
+    sequencia = cnpj[0] * len(cnpj)
 
-
-except IndexError as e:
-    print('Digite um CNPJ : ', e)
+    if sequencia == cnpj:
+        return True
+    else:
+        return False
 
 # 00.000.000/0000-00
-cnpj1 = '68.556.349/0001-48'
-valida(cnpj1)
+cnpj1 = input('Digite seu CNPJ: ')
+
+if valida(cnpj1):
+    print(f'{cnpj1}: É válido')
+else:
+    print(f'{cnpj1}: É inválido')
